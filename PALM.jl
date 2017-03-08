474f58a0ed115388014ec6f5a8d2dd8b8bd745ac
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-08 07:27:46 +0100

    Changes of structure for integration into COBRA.jl

--- PALM.jl
+++ PALM.jl
@@ -1,50 +1,11 @@
-# Configuration
-# Local directory with model files
-LOCAL_DIR_PATH = "./testModels" #/AGORA_fixedRxns_17_01_10_sorted_fixGPR
-MATLAB_EXEC = "/Applications/MATLAB_R2016b.app/bin/matlab"
-scriptName = "tutorial_modelCharact_script"
+#-------------------------------------------------------------------------------------------
+#=
+    Purpose:    Implementation of PALM.jl
+    Author:     Laurent Heirendt - LCSB - Luxembourg
+    Date:       March 2017
+=#
 
-include("tools.jl")
-
-# Number of MATLAB sessions
-nMatlab = 2
-
-# Part below is FROZEN - do not change unless you know what you are doing.
-if nMatlab == 1
-    error("The poolsize is equal to 1. PALM.jl is meant to be used in parallel, not serial or sequential.")
-end
-
-dirContent = readdir(LOCAL_DIR_PATH);
-nModels = length(dirContent);
-
-info("Directory read successfully ($nModels models).")
-
-nWorkers, realLoadRatio, restModels = shareLoad(nMatlab, nModels)
-
-# broadcast local variables to every worker
-globalVars = ["dirContent", "restModels", "realLoadRatio", "LOCAL_DIR_PATH", "scriptName"]
-
-for i = 1:length(globalVars)
-    eval(parse("@eval @everywhere $(globalVars[i]) = \$$(globalVars[i])"))
-end
-
-# define the names of the numerical characteristics to be retrieved
-varsCharact = ["Nmets", "Nrxns", "Nelem", "Nnz", "sparsityRatio", "bwidth", "maxVal", "minVal", "columnDensity", "rankA", "rankDeficiencyA", "maxSingVal", "minSingVal", "condNumber"]
-
-# determine the number of characteristics to be retrieved
-nCharacteristics = length(varsCharact)
-
-# prepare array for storing remote references
-R = Array{Future}(nWorkers)
-
-# declare an array to store the indices for each worker
-indicesWorkers = Array{Int}(nWorkers, 3)
-
-# declare an empty array for storing a summary of all data
-summaryData = Array{Union{Int,Float64,AbstractString}}(nModels + 1, nCharacteristics + 1)
-
-# use the MATLAB module on every worker
-@everywhere using MAT, MATLAB, COBRA
+#-------------------------------------------------------------------------------------------
 
 @everywhere function loopModels(p, scriptName, dirContent, startIndex, endIndex, nCharacteristics, varsCharact)
 
@@ -77,46 +38,67 @@ summaryData = Array{Union{Int,Float64,AbstractString}}(nModels + 1, nCharacteris
     end
 end
 
-# launch the function loopModels on every worker
-@sync for (p, pid) in enumerate(workers())
+function PALM(scriptName, nWorkers, varsCharact, outputFile)
 
-    info("Launching MATLAB session on worker $(p+1).")
-
-    startIndex = Int((p - 1) * realLoadRatio + 1)
-
-    # save the startIndex for each worker
-    indicesWorkers[p, 1] = startIndex
-
-    if p <  workers()[end]
-        endIndex = Int(p * realLoadRatio)
-        indicesWorkers[p, 2] = endIndex
-        indicesWorkers[p, 3] = realLoadRatio
-        info("Worker $(p+1) runs $realLoadRatio models: from $startIndex to $endIndex")
-    else
-        endIndex = Int((p - 1) * realLoadRatio + restModels)
-        indicesWorkers[p, 2] = endIndex
-        indicesWorkers[p, 3] = restModels
-        info("Worker $(p+1) runs $restModels models: from $startIndex to $endIndex")
+    # Part below is FROZEN - do not change unless you know what you are doing.
+    if nWorkers == 1
+        error("The poolsize is equal to 1. PALM.jl is meant to be used in parallel, not serial or sequential.")
     end
 
-    @async R[p] = @spawnat (p + 1) begin
-        loopModels(p, scriptName, dirContent, startIndex, endIndex, nCharacteristics, varsCharact)
+    # determine the number of characteristics to be retrieved
+    nCharacteristics = length(varsCharact)
+
+    # prepare array for storing remote references
+    R = Array{Future}(nWorkers)
+
+    # declare an array to store the indices for each worker
+    indicesWorkers = Array{Int}(nWorkers, 3)
+
+    # declare an empty array for storing a summary of all data
+    summaryData = Array{Union{Int,Float64,AbstractString}}(nModels + 1, nCharacteristics + 1)
+
+    # launch the function loopModels on every worker
+    @sync for (p, pid) in enumerate(workers())
+
+        info("Launching MATLAB session on worker $(p+1).")
+
+        startIndex = Int((p - 1) * realLoadRatio + 1)
+
+        # save the startIndex for each worker
+        indicesWorkers[p, 1] = startIndex
+
+        if p <  workers()[end]
+            endIndex = Int(p * realLoadRatio)
+            indicesWorkers[p, 2] = endIndex
+            indicesWorkers[p, 3] = realLoadRatio
+            info("Worker $(p+1) runs $realLoadRatio models: from $startIndex to $endIndex")
+        else
+            endIndex = Int((p - 1) * realLoadRatio + restModels)
+            indicesWorkers[p, 2] = endIndex
+            indicesWorkers[p, 3] = restModels
+            info("Worker $(p+1) runs $restModels models: from $startIndex to $endIndex")
+        end
+
+        @async R[p] = @spawnat (p + 1) begin
+            loopModels(p, scriptName, dirContent, startIndex, endIndex, nCharacteristics, varsCharact)
+        end
+
     end
 
+    # set the header of all the columns
+    summaryData[1, :] = [""; varsCharact]
+
+    # store the data retrieved from worker p
+    @sync for (p, pid) in enumerate(workers())
+        summaryData[indicesWorkers[p, 1] + 1:indicesWorkers[p, 2] + 1, :] = fetch(R[p][1:indicesWorkers[p, 3], :])
+    end
+
+    # save the summary data
+    fileName = outputFile#"modelCharacteristics.mat"
+    file = matopen(fileName, "w")
+    write(file, "summaryData", summaryData)
+
+    # close the file and return a status message
+    close(file)
+
 end
-
-# set the header of all the columns
-summaryData[1, :] = [""; varsCharact]
-
-# store the data retrieved from worker p
-@sync for (p, pid) in enumerate(workers())
-    summaryData[indicesWorkers[p, 1] + 1:indicesWorkers[p, 2] + 1, :] = fetch(R[p][1:indicesWorkers[p, 3], :])
-end
-
-# save the summary data
-fileName = "modelCharacteristics.mat"
-file = matopen(fileName, "w")
-write(file, "summaryData", summaryData)
-
-# close the file and return a status message
-close(file)
