3bc3a3758fd7aa3dce47328ed7c914b632bfe963
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-03 18:10:38 +0200

    generic submit fct

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -11,7 +11,7 @@ if [ "$ARCH" == "Linux" ]; then
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.clone(pwd()); Pkg.test(pwd(), coverage = true);'
 
         # adding coverage
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.add("Coverage"); cd(Pkg.dir("COBRA")); using Coverage; Codecov.submit(process_folder());'
+        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.add("Coverage"); cd(Pkg.dir("COBRA")); using Coverage; Codecov.submit_generic(process_folder());'
 
     elif [ "$JULIA_VER" == "v0.7.0" ]; then
         # temporary addition for julia 0.6 until new version of MAT tagged
