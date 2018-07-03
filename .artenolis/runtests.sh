587ae9db7d6eb9a59e3a184a6ca725853952fd25
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-03 14:02:21 +0200

    Debugging

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -2,7 +2,7 @@ echo "ARCH = $ARCH"
 echo "JULIA_VER = $JULIA_VER"
 
 if [ "$ARCH" == "Linux" ]; then
-    if [ "$JULIA_VER" == "0.6.3" ]; then
+    if [ "$JULIA_VER" == "v0.6.3" ]; then
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia -e 'println(versioninfo()); exit()'
 
         # temporary addition for julia 0.6 until new version of MAT tagged
@@ -12,7 +12,7 @@ if [ "$ARCH" == "Linux" ]; then
         #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.clone(pwd())'
         #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.build()'
         #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.test("COBRA",coverage=true)'
-    elif [ "$JULIA_VER" == "0.7.0" ]; then
+    elif [ "$JULIA_VER" == "v0.7.0" ]; then
         # temporary addition for julia 0.6 until new version of MAT tagged
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.add("MAT"); Pkg.checkout("MAT")'
 
