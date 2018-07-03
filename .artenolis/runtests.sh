53e4c155e562c40528332d32a62961978bf60f8e
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-03 14:13:56 +0200

    launch commands (1)

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -10,8 +10,8 @@ if [ "$ARCH" == "Linux" ]; then
 
         # add the COBRA module
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.clone(pwd())'
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.build()'
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.test("COBRA",coverage=true)'
+        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.build()'
+        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.test("COBRA",coverage=true)'
     elif [ "$JULIA_VER" == "v0.7.0" ]; then
         # temporary addition for julia 0.6 until new version of MAT tagged
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.add("MAT"); Pkg.checkout("MAT")'
