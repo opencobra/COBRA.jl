41e28d5cdb2d1472530799bcef40de91ae175ceb
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-03 16:09:55 +0200

    Cleaning up the artenolis script

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -3,18 +3,12 @@ echo "JULIA_VER = $JULIA_VER"
 
 if [ "$ARCH" == "Linux" ]; then
     if [ "$JULIA_VER" == "v0.6.3" ]; then
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia -e 'println(versioninfo()); exit()'
 
-        # temporary addition for julia 0.6 until new version of MAT tagged
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.add("MAT"); Pkg.checkout("MAT")'
         # remove th julia directory to clean the installation directory
         rm -rf ~/.julia/v0.6/COBRA
 
         # add the COBRA module
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.update(); Pkg.clone(pwd()); Pkg.test(pwd(), coverage = true);'
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.build()'
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'include("test/runtests.jl")'
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.rm(pwd())'
 
     elif [ "$JULIA_VER" == "v0.7.0" ]; then
         # temporary addition for julia 0.6 until new version of MAT tagged
