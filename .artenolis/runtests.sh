c96505addb2e770b4a0fde316361c4ebb23affee
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-03 17:42:56 +0200

    Cleaning up the artenolis script

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -8,19 +8,17 @@ if [ "$ARCH" == "Linux" ]; then
         rm -rf ~/.julia/v0.6/COBRA
 
         # add the COBRA module
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.update();'
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.clone(pwd());'
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.add(pwd());'
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.test(pwd(), coverage = true);'
 
     elif [ "$JULIA_VER" == "v0.7.0" ]; then
         # temporary addition for julia 0.6 until new version of MAT tagged
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.add("MAT"); Pkg.checkout("MAT")'
+        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.add("MAT"); Pkg.checkout("MAT")'
 
         # add the COBRA module
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.clone(pwd())'
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.build()'
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.test("COBRA",coverage=true)'
+        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.clone(pwd())'
+        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.build()'
+        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.test("COBRA",coverage=true)'
     fi
 fi
 
