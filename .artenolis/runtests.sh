7a3df367a8756349908ef718634e35a1d302aa8e
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-03 14:59:38 +0200

    change of julia install

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -3,16 +3,19 @@ echo "JULIA_VER = $JULIA_VER"
 
 if [ "$ARCH" == "Linux" ]; then
     if [ "$JULIA_VER" == "v0.6.3" ]; then
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia -e 'println(versioninfo()); exit()'
+        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia -e 'println(versioninfo()); exit()'
 
         # temporary addition for julia 0.6 until new version of MAT tagged
         #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.add("MAT"); Pkg.checkout("MAT")'
 
         # add the COBRA module
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.add(pwd())'
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.build()'
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.test(pwd(),coverage=true)'
+        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.build()'
+        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'include("test/runtests.jl")'
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.rm(pwd())'
+
+        # remove th julia directory to clean the installation directory
+        #rm -rf ~/.julia/v0.6
     elif [ "$JULIA_VER" == "v0.7.0" ]; then
         # temporary addition for julia 0.6 until new version of MAT tagged
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.add("MAT"); Pkg.checkout("MAT")'
@@ -27,5 +30,3 @@ fi
 CODE=$?
 exit $CODE
 
-# remove th julia directory to clean the installation directory
-rm -rf ~/.julia
