efc9b623cec6b1019c5a8aaab6a9d0d24ca8129e
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-04 16:23:39 +0200

    change of service name

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -13,7 +13,7 @@ if [ "$ARCH" == "Linux" ]; then
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --code-coverage=user --color=yes -e 'Pkg.clone(pwd()); Pkg.test(pwd(), coverage = true);'
 
         # adding coverage
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Coverage; Codecov.submit_generic(process_folder(), Pkg.dir("COBRA"), commit=$(ENV["GIT_COMMIT"]), branch=$(ENV["GIT_BRANCH"]));'
+        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Coverage; Codecov.submit_generic(process_folder(), Pkg.dir("COBRA"), service="artenolis", commit=$(ENV["GIT_COMMIT"]), branch=$(ENV["GIT_BRANCH"]));'
         #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'cd(Pkg.dir("COBRA")); using Coverage; Codecov.submit_generic(process_folder(), service="artenolis", branch=ENV["GIT_BRANCH"], commit=ENV["GIT_COMMIT"]);'
 
     elif [ "$JULIA_VER" == "v0.7.0" ]; then
