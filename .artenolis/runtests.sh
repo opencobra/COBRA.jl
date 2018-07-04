d1dc5b70fb3dc9a21b6b16bb8929defa007b8f66
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-04 14:06:17 +0200

    Fix for codecov token

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -1,6 +1,18 @@
+## packages to be installed on the node
+# Pkg.add("Coverage");
+# Pkg.add("Clp");
+# Pkg.add("CPLEX");
+# Pkg.add("Gurobi");
+# Pkg.add("Cbc");
+# Pkg.add("MAT");
+# Pkg.add("Mosek");
+# Pkg.add("MATLAB");
+
+# print out the version and the architecture
 echo "ARCH = $ARCH"
 echo "JULIA_VER = $JULIA_VER"
 
+# launch the test script
 if [ "$ARCH" == "Linux" ]; then
     if [ "$JULIA_VER" == "v0.6.3" ]; then
 
@@ -11,7 +23,7 @@ if [ "$ARCH" == "Linux" ]; then
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.clone(pwd()); Pkg.test(pwd(), coverage = true);'
 
         # adding coverage
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.add("Coverage"); cd(Pkg.dir("COBRA")); using Coverage; Codecov.submit_generic(process_folder());'
+        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Coverage; Codecov.submit_generic(process_folder(), codecov_url="https://codecov.io");'
 
     elif [ "$JULIA_VER" == "v0.7.0" ]; then
         # temporary addition for julia 0.6 until new version of MAT tagged
