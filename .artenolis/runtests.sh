885f013aa46b8d2ad18ee9b487930c85574b0f9b
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-05 14:26:19 +0200

    cleanup before merge

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -13,13 +13,8 @@ if [ "$ARCH" == "Linux" ]; then
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.clone(pwd()); cd(Pkg.dir("COBRA")); Pkg.test(pwd(), coverage=true);'
 
     elif [ "$JULIA_VER" == "v0.7.0" ]; then
-        # temporary addition for julia 0.6 until new version of MAT tagged
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.add("MAT"); Pkg.checkout("MAT")'
-
         # add the COBRA module
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.clone(pwd())'
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.build()'
-        #/mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.test("COBRA",coverage=true)'
     fi
 fi
 
