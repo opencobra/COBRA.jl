9ecf97c852a611f2c313a81afa7e94f8d50a01e6
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-13 11:58:49 +0200

    Cleaning up

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -18,10 +18,6 @@ if [ "$ARCH" == "Linux" ]; then
     fi
 
 elif [ "$ARCH" == "windows" ]; then
-    # change to the build directory
-    #echo " -- changing to the build directory --"
-    #cd "D:\\jenkins\\workspace\\$CI_PROJECT_NAME\\JULIA_VER\\$JULIA_VER\\\\MATLAB_VER\\$MATLAB_VER\\label\\COBRA"
-
     # remove th julia directory to clean the installation directory
     rm -rf ~/.julia/v0.6/COBRA
 
