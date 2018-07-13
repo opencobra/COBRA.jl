c7561e3ddce1529f99aab5fb438f066c4bc6108e
laurentheirendt <laurent.heirendt@uni.lu>
2018-07-13 11:22:26 +0200

    draft script of artenolis script for windows

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -16,6 +16,23 @@ if [ "$ARCH" == "Linux" ]; then
         # add the COBRA module
         /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.clone(pwd())'
     fi
+
+elif [ "$ARCH" == "windows" ]; then
+    # change to the build directory
+    echo " -- changing to the build directory --"
+    cd "D:\\jenkins\\workspace\\$CI_PROJECT_NAME\\JULIA_VER\\$JULIA_VER\\\\MATLAB_VER\\$MATLAB_VER\\label\\$ARCHVERSION"
+
+    # remove th julia directory to clean the installation directory
+    rm -rf ~/.julia/v0.6/COBRA
+
+    unset Path
+    nohup "D:\\JULIA\\$JULIA_VER\\\bin\\julia.exe" --color=yes -e 'Pkg.clone(pwd()); cd(Pkg.dir("COBRA")); Pkg.test(pwd(), coverage=true);' > output.log & PID=$!
+
+    # follow the log file
+    tail -n0 -F --pid=$! output.log 2>/dev/null
+
+    # wait until the background process is done
+    wait $PID
 fi
 
 CODE=$?
