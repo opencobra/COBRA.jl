a633377a97c365ae7880231e9ec69c01e8d57d49
laurentheirendt <laurent.heirendt@uni.lu>
2018-08-05 18:15:04 +0200

    test scriptfile

---
+++ test/scriptFile.m
@@ -0,0 +1,19 @@
+initCobraToolbox(0)
+
+% define the filename of the model
+modelFile = PALM_modelFile;
+
+% define the fieldname of the stoichiometric matrix
+matrixName = 'S';
+
+model = readCbModel([PALM_dir filesep modelFile]);
+
+S = model.S;
+
+[nMets, nRxns] = size(S)
+
+% determine the number of elements in S
+nElem = numel(S)
+
+% determine the number of nonzero elements in S
+nNz = nnz(S)
