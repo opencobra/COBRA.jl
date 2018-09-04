0722b690f37074edd3270a0daa5f3a93fd090e84
laurentheirendt <laurent.heirendt@uni.lu>
2018-09-04 13:22:31 +0200

    Testing silent CPLEX

--- test/p_all.jl
+++ test/p_all.jl
@@ -47,10 +47,15 @@ model = loadModel("$(Pkg.dir("COBRA"))/test/ecoli_core_model.mat", "S", "model")
 
 # test that no output is produced with printLevel = 0 (only for Gurobi)
 if string(solverName) == "Gurobi"
-    info("Testing silent Gurobi")
+    info(" > Testing silent Gurobi")
     solver = changeCobraSolver(solverName, printLevel=0)
     output = @capture_out minFlux, maxFlux = distributedFBA(model, solver, nWorkers=nWorkers, printLevel=0, rxnsList=1:4)
     @test length(matchall(r"From worker ", output)) == 2*nWorkers
+elseif string(solverName) == "CPLEX"
+    info(" > Testing silent CPLEX")
+    solver = changeCobraSolver(solverName, printLevel=0)
+    output = @capture_out minFlux, maxFlux = distributedFBA(model, solver, nWorkers=nWorkers, printLevel=0, rxnsList=1:4)
+    @test length(output) == 0
 end
 
 # change the COBRA solver
