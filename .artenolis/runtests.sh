5894e942b89ca502ee2ccb41d2a712a84a1ac51d
laurentheirendt <laurent.heirendt@uni.lu>
2019-03-21 15:16:33 +0100

    adding env vars in shell script

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -7,14 +7,14 @@ echo "MATLAB_VER = $MATLAB_VER"
 if [ "$ARCH" == "Linux" ]; then
     if [ "$JULIA_VER" == "v0.6.4" ]; then
         # remove th julia directory to clean the installation directory
-        rm -rf ~/.julia/v0.6/COBRA
+        rm -rf $JULIA_PKGDIR/v0.6/COBRA
 
         # add the COBRA module
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'Pkg.clone(pwd()); cd(Pkg.dir("COBRA")); Pkg.test(pwd(), coverage=true);'
+        $ARTENOLIS_SOFT_PATH/julia/$JULIA_VER/bin/julia --color=yes -e 'Pkg.clone(pwd()); cd(Pkg.dir("COBRA")); Pkg.test(pwd(), coverage=true);'
 
     elif [ "$JULIA_VER" == "v0.7.0" ]; then
         # add the COBRA module
-        /mnt/prince-data/JULIA/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.clone(pwd())'
+        $ARTENOLIS_SOFT_PATH/julia/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.clone(pwd())'
     fi
 
 elif [ "$ARCH" == "macOS" ]; then
