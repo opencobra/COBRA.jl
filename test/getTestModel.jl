f542087d30147018771b31793bf5f99b0fa47166
laurentheirendt <laurent.heirendt@uni.lu>
2019-04-19 09:34:31 +0200

    change url of test model

--- test/getTestModel.jl
+++ test/getTestModel.jl
@@ -2,7 +2,7 @@ function getTestModel()
 
     if !isfile("ecoli_core_model.mat")
         print("Downloading the ecoli_core model ...")
-        ecoliModel = get("http://gcrg.ucsd.edu/sites/default/files/Attachments/Images/downloads/Ecoli_core/ecoli_core_model.mat")
+        ecoliModel = get("https://github.com/LCSB-BioCore/COBRA.models/raw/master/mat/ecoli_core_model.mat")
         save(ecoliModel, "ecoli_core_model.mat")
         print_with_color(:green, "Done.\n")
     else
