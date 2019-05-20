f9c5a9a193344d75578087c6939d40197b362ccc
laurentheirendt <laurent.heirendt@uni.lu>
2019-05-20 15:55:05 +0200

    comment rmdir

--- test/runtests.jl
+++ test/runtests.jl
@@ -138,7 +138,7 @@ end
 # remove the results folder to clean up
 tmpDir = joinpath(dirname(@__FILE__), "..", "results")
 if isdir(tmpDir)
-    rm(tmpDir, force=true, recursive=true)
+    #rm(tmpDir, force=true, recursive=true)
 end
 
 # print a status line
