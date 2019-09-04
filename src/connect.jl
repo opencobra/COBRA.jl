239c4b66f51cfa9205b8fc7e749430ef3289b586
laurentheirendt <laurent.heirendt@uni.lu>
2019-09-04 19:45:03 +0200

    change to sys bindir in test

--- src/connect.jl
+++ src/connect.jl
@@ -9,7 +9,7 @@
 
 if "JENKINS" in keys(ENV)
     @info "JENKINS CI server detected. Workers will be added with test environment configuration."
-    include(ENV["ARTENOLIS_SOFT_PATH"]*"/julia/"*ENV["JULIA_VER"]*"/share/julia/test/testenv.jl")
+    include(Sys.BINDIR*"/../share/julia/test/testenv.jl")
     addprocsCOBRA = addprocs_with_testenv
 else
     addprocsCOBRA = addprocs
